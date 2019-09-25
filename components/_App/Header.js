4abfbf57775dac9ea1c6add4e8ca3f68b61b2535
shaho <toofani@gmail.com>
2019-09-26 01:05:22 +0330

    Add header and its dependencies

---
+++ components/_App/Header.js
@@ -0,0 +1,76 @@
+import Link from "next/link";
+import { Menu, Container, Image, Icon } from "semantic-ui-react";
+
+const Header = () => {
+  const user = false;
+
+  return (
+    <Menu fluid id="menu" inverted>
+      <Container text>
+        <Link href="/">
+          <Menu.Item header>
+            <Image
+              size="mini"
+              src="/static/logo.svg"
+              style={{
+                marginRight: "1em",
+              }}
+            />
+            ReactReserve
+          </Menu.Item>
+        </Link>
+        <Link href="/cart">
+          <Menu.Item header>
+            <Icon name="cart" size="large" />
+            Cart
+          </Menu.Item>
+        </Link>
+
+        {/* User */}
+        {user && (
+          <Link href="/create">
+            <Menu.Item header>
+              <Icon name="add square" size="large" />
+              Create
+            </Menu.Item>
+          </Link>
+        )}
+
+        {/* Login or Logout */}
+        {user ? (
+          <>
+            <Link href="/account">
+              <Menu.Item header>
+                <Icon name="user" size="large" />
+                Account
+              </Menu.Item>
+            </Link>
+
+            <Menu.Item header>
+              <Icon name="sign out" size="large" />
+              Logout
+            </Menu.Item>
+          </>
+        ) : (
+          <>
+            <Link href="/login">
+              <Menu.Item header>
+                <Icon name="sign in" size="large" />
+                Login
+              </Menu.Item>
+            </Link>
+
+            <Link href="/signup">
+              <Menu.Item header>
+                <Icon name="signup" size="large" />
+                Signup
+              </Menu.Item>
+            </Link>
+          </>
+        )}
+      </Container>
+    </Menu>
+  );
+};
+
+export default Header;
