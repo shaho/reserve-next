12b49a8ca31551644543d0d9ff39665cbc424e70
shaho <toofani@gmail.com>
2019-10-10 00:50:05 +0330

    Implement deleting a product in both client and server

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,6 +1,24 @@
-import { Header, Button } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Header, Button, Modal } from "semantic-ui-react";
+import axios from "axios";
+import { useRouter } from "next/router";
 
-function ProductAttributes({ description }) {
+import baseUrl from "../../utils/baseUrl";
+
+function ProductAttributes({ _id, description }) {
+  const router = useRouter();
+  // ─── STATES ─────────────────────────────────────────────────────────────────────
+  const [modal, setModal] = useState(false);
+
+  // ─── HANDLERS ───────────────────────────────────────────────────────────────────
+  const handleDelete = async () => {
+    const url = `${baseUrl}/api/product`;
+    const payload = { params: { _id } };
+    await axios.delete(url, payload);
+    router.push("/");
+  };
+
+  // ─── JSX ────────────────────────────────────────────────────────────────────────
   return (
     <>
       <Header as="h3">About this product</Header>
@@ -9,7 +27,25 @@ function ProductAttributes({ description }) {
         icon="trash alternate outline"
         color="red"
         content="Delete Product"
+        onClick={() => setModal(true)}
       />
+
+      <Modal open={modal} dimmer={"blurring"}>
+        <Modal.Header>Confirm Delete</Modal.Header>
+        <Modal.Content>
+          <p>Are you sure you want to delete this product?</p>
+          <Modal.Actions>
+            <Button content="Cancel" onClick={() => setModal(false)} />
+            <Button
+              negative
+              icon="trash"
+              labelPosition="right"
+              content="Delete"
+              onClick={handleDelete}
+            />
+          </Modal.Actions>
+        </Modal.Content>
+      </Modal>
     </>
   );
 }
--- pages/api/product.js
+++ pages/api/product.js
@@ -4,11 +4,38 @@ import connectDb from "../../utils/connectDb";
 connectDb();
 
 export default async (request, response) => {
+  switch (request.method) {
+    case "GET":
+      await handleGetRequest(request, response);
+      break;
+
+    case "DELETE":
+      await handleDeleteRequest(request, response);
+      break;
+
+    default:
+      response.status(405).send(`Method ${request.method} not allowed`);
+      break;
+  }
+};
+
+// ─── HANDLE GET REQUEST ─────────────────────────────────────────────────────────
+const handleGetRequest = async (request, response) => {
   try {
     const { _id } = request.query;
     const product = await Product.findById(_id);
     response.status(200).json(product);
-    // console.log(product);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+// ─── HANDLE DELETE REQUEST ──────────────────────────────────────────────────────
+const handleDeleteRequest = async (request, response) => {
+  try {
+    const { _id } = request.query;
+    await Product.findByIdAndDelete(_id);
+    response.status(204).json({});
   } catch (error) {
     console.error(error.message);
   }
