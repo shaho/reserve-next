dc48c5c6861c6d84485b12b4188e4527543c724c
shaho <toofani@gmail.com>
2019-10-09 23:55:19 +0330

    Add baseUrl helper

--- pages/product.js
+++ pages/product.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+
 import ProductSummary from "../components/Product/ProductSummary";
 import ProductAttributes from "../components/Product/ProductAttributes";
-import AddProductToCart from "../components/Product/AddProductToCart";
+import baseUrl from "../utils/baseUrl";
 
 const Product = ({ product }) => {
   return (
@@ -13,7 +14,7 @@ const Product = ({ product }) => {
 };
 
 Product.getInitialProps = async ({ query: { _id } }) => {
-  const url = "http://localhost:3000/api/product";
+  const url = `${baseUrl}/api/product`;
   const payload = { params: { _id } };
   const response = await axios.get(url, payload);
   return {
