e8e53691aebcf905b374da5387024f19664ab825
shaho <toofani@gmail.com>
2019-10-14 16:58:11 +0330

    Fix invalid Auth tokens in _app.js by removing the cookie and redirect user to login page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import App from "next/app";
-import { parseCookies } from "nookies";
+import { parseCookies, destroyCookie } from "nookies";
 import axios from "axios";
 
 import Layout from "../components/_App/Layout";
@@ -33,6 +33,10 @@ class MyApp extends App {
         pageProps.user = user;
       } catch (error) {
         console.error("Error getting current user", error);
+
+        // Handling inavlid token
+        destroyCookie(ctx, "token");
+        redirectUser(ctx, "/login");
       }
     }
 
